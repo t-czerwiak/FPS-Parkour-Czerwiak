6665f06263b9cf2bf66b052f9b54ed5150697e10
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-05-16 09:03:22 -0300

    terminado

---
+++ Assets/RestartSceneOnFall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartSceneOnFall : MonoBehaviour
+{
+    public float deathFallHeight = -15;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.y < deathFallHeight)
+        {
+            Debug.Log("Respawn");
+            SceneManager.LoadScene("FPS Parkour");
+        }
+    }
+}
