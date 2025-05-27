a4087f8e3fc8dc87432bd3f9bf7b2633ba23d4d3
t-czerwiak <timoczerwiak@gmail.com>
2025-05-27 11:12:35 -0300

    cambiado a unity 2022

---
+++ Assets/DeteccionDeColisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Contacto");
+		Destroy(gameObject);
+	}
+}
